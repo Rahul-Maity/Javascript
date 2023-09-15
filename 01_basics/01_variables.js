41ebc9ebc5639b3285ec9fb027b5ad14559d81cc
Rahul Maity <113348978+Rahul-Maity@users.noreply.github.com>
2023-09-15 14:12:59 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=144553
+let accounntEmail="rahul@google.com"
+var accountPassword="212153"
+accountCity="kolkata"
+
+// console.log(accountCity)
+console.table([accountId,accounntEmail,accountPassword,accountPassword])
